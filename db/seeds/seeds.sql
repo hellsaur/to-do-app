61384039b08f453efeae5d6adcc2de5951328e83
marco acosta <marcucho14@hotmail.com>
2017-10-24 21:42:41 -0400

    update boilerplate

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,1 +1,1 @@
-INSERT INTO task (title, description, category, difficulty) VALUES ('homework', 'to be done thursday','HW',8);
+INSERT INTO task (title, description, done) VALUES ('homework', 'to be done thursday',FALSE);
